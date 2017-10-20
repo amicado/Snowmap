5a339e35ef48409b8496633aa18df5957b1176fa
Thomas Anderl <thomas.anderl@aon.at>
2017-10-20 02:16:45 +0200

    Precaching sounds

--- Game/scripts/vscripts/addon_game_mode.lua
+++ Game/scripts/vscripts/addon_game_mode.lua
@@ -18,13 +18,13 @@ require( "utility_functions" )
 --require( "events" )
 
 function Precache( context )
-	--[[
-		Precache things we know we'll use.  Possible file types include (but not limited to):
-			PrecacheResource( "model", "*.vmdl", context )
-			PrecacheResource( "soundfile", "*.vsndevts", context )
-			PrecacheResource( "particle", "*.vpcf", context )
-			PrecacheResource( "particle_folder", "particles/folder", context )
-	]]
+
+	--Precache things we know we'll use.  Possible file types include (but not limited to):
+	--PrecacheResource( "model", "*.vmdl", context )
+	--PrecacheResource( "particle", "*.vpcf", context )
+	--PrecacheResource( "particle_folder", "particles/folder", context )
+	PrecacheResource("soundfile", "soundevents/custom_sounds.vsndevts", context)
+
 end
 
 -- Create the game mode when we activate
