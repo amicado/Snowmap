51cf6d60dd8dabe3cca9d865728d52d7f7e007f6
Thomas Anderl <thomas.anderl@aon.at>
2017-10-30 16:46:46 +0100

    Sounds

--- Game/scripts/vscripts/entity_functions.lua
+++ Game/scripts/vscripts/entity_functions.lua
@@ -1,7 +1,12 @@
 function Trigger_Sound(trigger)
     print (trigger.activator:GetOwner():GetPlayerID())
     --EmitAnnouncerSoundForPlayer("dsadowski_01.music.battle_01",trigger.activator:GetOwner():GetPlayerID());
-    EmitGlobalSound("frostivus_awaits_you");
-    EmitAnnouncerSound("frostivus_awaits_you")
-    StartSoundEvent("frostivus_awaits_you",trigger.activator:GetOwner())
+    --trigger.caller:EmitSound("Courier.Spawn");
+    --EmitAnnouncerSound("frostivus_awaits_you")
+	--StartSoundEvent("frostivus_awaits_you",trigger.activator:GetOwner())
+	--EmitSoundOn("frostivus_awaits_you",trigger.activator:GetOwner())
+	EmitSoundOnClient("frostivus_awaits_you",trigger.activator:GetPlayerOwner());
+	--EmitAnnouncerSoundForPlayer("test",0)
+	--CustomGameEventManager:Send_ServerToAllClients( "emit_client_sound", {sound = "General.Coins"} )
+	--EmitAnnouncerSound("announcer_announcer_roshan_fallen_dire")
 end
