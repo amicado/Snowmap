3fba845eb5e39869674166471d2e1af0d4b85915
Thomas Anderl <thomas.anderl@aon.at>
2017-11-08 14:54:49 +0100

    Fixed all triggers at spawns

--- Game/scripts/vscripts/entity_functions.lua
+++ Game/scripts/vscripts/entity_functions.lua
@@ -1,12 +1,12 @@
-function Trigger_Sound(trigger)
+function Leave_Spawn(trigger)
     print (trigger.activator:GetOwner():GetPlayerID())
     --EmitAnnouncerSoundForPlayer("dsadowski_01.music.battle_01",trigger.activator:GetOwner():GetPlayerID());
     --trigger.caller:EmitSound("Courier.Spawn");
     --EmitAnnouncerSound("frostivus_awaits_you")
 	--StartSoundEvent("frostivus_awaits_you",trigger.activator:GetOwner())
 	--EmitSoundOn("frostivus_awaits_you",trigger.activator:GetOwner())
-	--EmitSoundOnClient("frostivus_awaits_you",trigger.activator:GetPlayerOwner());
-	EmitAnnouncerSoundForPlayer("frostivus_awaits_you",trigger.activator:GetPlayerOwner():GetPlayerID())
+	EmitSoundOnClient("frostivus_awaits_you",trigger.activator:GetPlayerOwner());
+	--EmitAnnouncerSoundForPlayer("frostivus_awaits_you",trigger.activator:GetPlayerOwner():GetPlayerID())
 	--CustomGameEventManager:Send_ServerToAllClients( "emit_client_sound", {sound = "General.Coins"} )
 	--EmitAnnouncerSound("announcer_announcer_roshan_fallen_dire")
 end
