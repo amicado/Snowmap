c779ddd0d6d7f02f18c1769f94893b8ca5956d28
Thomas Anderl <thomas.anderl@aon.at>
2017-11-23 00:30:51 +0100

    Fix to custom sound

--- Game/scripts/vscripts/entity_functions.lua
+++ Game/scripts/vscripts/entity_functions.lua
@@ -2,14 +2,15 @@ function Leave_Spawn(trigger)
 	--print("Leave_Spawn");
     --print (trigger.activator:GetOwner():GetPlayerID())
     --EmitAnnouncerSoundForPlayer("dsadowski_01.music.battle_01",trigger.activator:GetOwner():GetPlayerID());
-    trigger.caller:EmitSoundOnClient("spawn_trigger",trigger.activator:GetOwner());
+    --trigger.caller:EmitSoundOnClient("spawn_trigger",trigger.activator:GetOwner());
     --EmitAnnouncerSound("frostivus_awaits_you")
 	--StartSoundEvent("frostivus_awaits_you",trigger.activator:GetOwner())
 	--EmitSoundOn("frostivus_awaits_you",trigger.activator:GetOwner())
 	--EmitSoundOnClient("frostivus_awaits_you",trigger.activator:GetPlayerOwner());
 	--EmitAnnouncerSoundForPlayer("frostivus_awaits_you",trigger.activator:GetPlayerOwner():GetPlayerID())
 	--CustomGameEventManager:Send_ServerToAllClients( "emit_sound", {sound = ""} );
-	--EmitAnnouncerSound("announcer_announcer_roshan_fallen_dire")
+	CustomGameEventManager:Send_ServerToPlayer(trigger.activator:GetPlayerOwner(), "emit_sound", {sound = "spawn_trigger"} );
+	--EmitAnnouncerSound("spawn_trigger")
 end
 
 function PresentAutoCast()
