0c4ca3973f0e84d8d3057bb9c73d8b3957d2ccaf
Thomas Anderl <thomas.anderl@aon.at>
2017-11-23 00:02:51 +0100

    New values

--- Game/scripts/vscripts/entity_functions.lua
+++ Game/scripts/vscripts/entity_functions.lua
@@ -2,13 +2,13 @@ function Leave_Spawn(trigger)
 	--print("Leave_Spawn");
     --print (trigger.activator:GetOwner():GetPlayerID())
     --EmitAnnouncerSoundForPlayer("dsadowski_01.music.battle_01",trigger.activator:GetOwner():GetPlayerID());
-    --trigger.caller:EmitSound("Courier.Spawn");
+    trigger.caller:EmitSoundOnClient("spawn_trigger",trigger.activator:GetOwner());
     --EmitAnnouncerSound("frostivus_awaits_you")
 	--StartSoundEvent("frostivus_awaits_you",trigger.activator:GetOwner())
 	--EmitSoundOn("frostivus_awaits_you",trigger.activator:GetOwner())
 	--EmitSoundOnClient("frostivus_awaits_you",trigger.activator:GetPlayerOwner());
 	--EmitAnnouncerSoundForPlayer("frostivus_awaits_you",trigger.activator:GetPlayerOwner():GetPlayerID())
-	CustomGameEventManager:Send_ServerToAllClients( "emit_sound", {sound = "frostivus_awaits_you"} );
+	--CustomGameEventManager:Send_ServerToAllClients( "emit_sound", {sound = ""} );
 	--EmitAnnouncerSound("announcer_announcer_roshan_fallen_dire")
 end
 
